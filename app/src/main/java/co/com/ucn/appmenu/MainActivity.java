616a5a224439a8af94d294a1d19f2f2c680bb65f
Maria Orozco <mariozme@suramericana.con.co>
2024-11-12 18:55:23 -0500

    Se modifina la navegación

--- app/src/main/java/co/com/ucn/appmenu/MainActivity.java
+++ app/src/main/java/co/com/ucn/appmenu/MainActivity.java
@@ -1,6 +1,8 @@
 package co.com.ucn.appmenu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ImageButton menuBoton = (ImageButton) findViewById(R.id.menuButton);
+
+        menuBoton.setOnClickListener((v) -> {
+            Intent  intencion = new Intent(getApplicationContext(), MenuPrincipalActivity.class);
+            startActivity(intencion);
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/co/com/ucn/appmenu/MenuPrincipalActivity.java
+++ app/src/main/java/co/com/ucn/appmenu/MenuPrincipalActivity.java
@@ -1,14 +1,19 @@
 package co.com.ucn.appmenu;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -16,7 +21,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import co.com.ucn.appmenu.databinding.ActivityMenuPrincipalBinding;
 
-public class MenuPrincipalActivity extends AppCompatActivity {
+public class MenuPrincipalActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMenuPrincipalBinding binding;
@@ -42,7 +47,7 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_perfil, R.id.nav_citas, R.id.nav_calendario)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_menu_principal);
@@ -62,5 +67,24 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_menu_principal);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_menu_principal);
+
+        if (id == R.id.nav_perfil) {
+            navController.navigate(R.id.nav_perfil);
+        } else if (id == R.id.nav_citas) {
+            navController.navigate(R.id.nav_citas);
+        } else if (id == R.id.nav_calendario) {
+            navController.navigate(R.id.nav_calendario);
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
